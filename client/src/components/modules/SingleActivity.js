971f4a12694477c66966131fd9935fabb17a7366
许霖 <l-xu22@mails.tsinghua.edu.cn>
2024-02-02 23:23:01 +0800

    初步设计了activity卡片的样式，弄了一个样例

--- client/src/components/modules/SingleActivity.js
+++ client/src/components/modules/SingleActivity.js
@@ -5,6 +5,7 @@ import "../../utilities.css";
 import "./SingleActivity.css";
 
 // 7. The register button of the activity
+//Check the import is right or not for the new files.@xvlincaigou 2024/2/2
 
 //To simple users, I think that the score, download and remark functions of the activity should be put in the homepage rather than in the activity page.
 //To activity managers, I think that the functions of the activity should not be put in the activity page.
@@ -27,7 +28,18 @@ import "./SingleActivity.css";
 const SingleActivity = (props) => {
     return (
         <div className="Activity-container">
-            
+            <span>{props.name}</span>
+            <span>举办时间：{props.held_time}</span>
+            <span>报名截止时间：{props.latest_register_time}</span>
+            <p className="Activity-information">{props.information}</p>
+            <span>已报名人数：{props.number_of_people_signed_up}</span>
+            <div>
+                <span>已报名的用户：</span>
+                {props.users_signed_up.map((user) => (
+                    <span className="Activity-user-signed-up">{user}</span>
+                ))}
+            </div>
+            <span>平均评分：{props.average_score}</span>
         </div>
     );
 }
--- client/src/components/pages/Activity.js
+++ client/src/components/pages/Activity.js
@@ -4,6 +4,7 @@ import { get } from "../../utilities";
 import "../../utilities.css";
 import "./Activity.css";
 import "../modules/SingleActivity.js";
+import SingleActivity from "../modules/SingleActivity.js";
 
 //To simple users, I think that the score, download and remark functions of the activity should be put in the homepage rather than in the activity page.
 //To activity managers, I think that the functions of the activity should not be put in the activity page.
@@ -11,7 +12,15 @@ import "../modules/SingleActivity.js";
 
 const Activity = (props) => {
     return (
-        <></>
+        <SingleActivity 
+        name="建院外包开发"
+        held_time="2024/2/2"
+        latest_register_time="2024/2/2"
+        information="这是一个测试"
+        number_of_people_signed_up={4}
+        users_signed_up={["user1", "user2", "user3", "user4"]}
+        average_score={100}
+        />
     );
 }
 
