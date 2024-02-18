f98eea661268457411cc6d68da592284d496a228
许霖 <l-xu22@mails.tsinghua.edu.cn>
2024-02-18 11:41:13 +0800

    写了一部分关于管理员管理活动的前端代码，但是由于API的不明确，目前没有对接好

--- client/src/components/modules/SingleActivity.js
+++ client/src/components/modules/SingleActivity.js
@@ -1,4 +1,4 @@
-import React , { useState , useEffect }from "react";
+import React , { useState , useEffect } from "react";
 
 import "../../utilities.css";
 import "./SingleActivity.css";
@@ -9,34 +9,33 @@ import { post } from "../../utilities.js";
 
 const SingleActivity = (props) => {
   
-    const [inOrOut, setInOrOut] = useState(false);
-    const [button, setButton] = useState(null);
+    const [button, setButton] = useState(0);
     const [time, setTime] = useState(null);
+    const [users_signed_up, setUsers_signed_up] = useState(props.users_signed_up);
 
     useEffect(() => {
         const currentDateTime = new Date();
         const start_time = new Date(props.start_time);
         const end_time = new Date(props.end_time);
         const latest_register_time = new Date(props.latest_register_time);
-        const candidate = {u_id:props.user.u_id, name:props.user.name};
-        setInOrOut(candidate in props.users_signed_up);
+
+        let usersSignedUpIds = props.users_signed_up.map(user => user.u_id);
+        let inorout = usersSignedUpIds.includes(props.user.u_id);
 
         if (latest_register_time > currentDateTime) {//活动可以报名
             setTime(<div className="Activity-held-time Activity-held-time-color1">{convertToBeijingTime(props.start_time) + " ~ " + convertToBeijingTime(props.end_time)}</div>);
-            setButton(<ActivityRegisterButton inOrOut={inOrOut} handleClick={handleClick}/>);
+            inorout ? setButton(11) : setButton(10);
         } else if (start_time > currentDateTime) {//活动报名结束但是没有开始
             setTime(<div className="Activity-held-time Activity-held-time-color2">{convertToBeijingTime(props.start_time) + " ~ " + convertToBeijingTime(props.end_time)}</div>);
+            setButton(2);
         } else if (end_time > currentDateTime){//活动正在进行
             setTime(<div className="Activity-held-time Activity-held-time-color3">{convertToBeijingTime(props.start_time) + " ~ " + convertToBeijingTime(props.end_time)}</div>);
+            setButton(3);
         } else {//活动结束
             setTime(<div className="Activity-held-time Activity-held-time-color4">{convertToBeijingTime(props.start_time) + " ~ " + convertToBeijingTime(props.end_time)}</div>);
-            //if (props.users_admin.some(user => user == candidate) || props.supervisors.some(user => user == candidate)) {
-                setButton(<><ActivityDownloadButton uid={candidate.u_id} aid={props._id}/>
-                <ActivityRemarkButton creator={candidate} activity_id={props._id}/></>);
-            //}
-            console.log(props.users_admin);
-            console.log(props.supervisors);
-            console.log(candidate);
+            let usersAdminIds = props.users_admin.map(user => user.u_id);
+            let supervisorsIds = props.supervisors.map(user => user.u_id);
+            (supervisorsIds.includes(props.user.u_id) || usersAdminIds.includes(props.user.u_id)) ? setButton(41) : setButton(40);
         }
     }, []);
 
@@ -57,11 +56,19 @@ const SingleActivity = (props) => {
       }
     
     const handleClick = () => {
-        if (inOrOut) {
-            post("/api/activity/unsubscribe", {uid: props.user.u_id, aid:props._id}).then((res) => {alert(res);setInOrOut(!inOrOut)}).catch((err) => {alert(err);});
+        if (button == 11) {
+            post("/api/activity/unsubscribe", {uid: props.user.u_id, aid:props._id}).then((res) => {
+                alert(res);
+                setButton(10);
+                const newUsers_signed_up = users_signed_up.filter((user) => user.u_id !== props.user.u_id);
+                setUsers_signed_up(newUsers_signed_up);})
+            .catch((err) => {alert(err);});
         } else {
-            post("/api/activity/register", {email: props.user.u_id, activity_id:props._id, name: props.user.name}).then((res) => 
-            {alert(res.message);setInOrOut(!inOrOut);setButton(<ActivityRegisterButton inOrOut={inOrOut} handleClick={handleClick}/>);}).catch((err) => {alert(err);});
+            post("/api/activity/subscribe", {uid: props.user.u_id, aid:props._id}).then((res) => {   
+                alert(res.message);
+                setButton(11);
+                setUsers_signed_up([...users_signed_up, {u_id: props.user.u_id, name: props.user.name}]);})
+            .catch((err) => {alert(err);});
         }
     };
 
@@ -69,7 +76,14 @@ const SingleActivity = (props) => {
         <div className="Activity-container">
             {time}
             <div className="Activity-Content">{props.name}</div>
-            {button}
+            {
+                //用一个数字表示按钮被渲染的形态。10：不在里面；11：在里面；2，3，40：啥都不渲染；41：在里面
+                button == 10 ? <ActivityRegisterButton inOrOut={false} handleClick={handleClick}/> :
+                button == 11 ? <ActivityRegisterButton inOrOut={true} handleClick={handleClick}/> :
+                button == 41 ? <><ActivityDownloadButton uid={props.user.u_id} aid={props._id}/>
+                <ActivityRemarkButton creator={{u_id: props.user.u_id, name: props.user.name}} activity_id={props._id}/></> :
+                null
+            }
             <div className="Activity-infoSection">
                 <div className="Activity-infoBody">地点{" | " + props.location}</div>
                 <div className="Activity-infoBody">报名截止时间{" | " + convertToBeijingTime(props.latest_register_time)}</div>
@@ -84,7 +98,7 @@ const SingleActivity = (props) => {
                 </div>
                 <div className="Activity-infoBody">
                     <span>已报名的用户 | </span>
-                    {props.users_signed_up.map((user) => (
+                    {users_signed_up.map((user) => (
                         <span>{user.name + "  "}</span>
                     ))}
                 </div>
