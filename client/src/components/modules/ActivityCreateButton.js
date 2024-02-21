bb956e8d1bfe9554b8a9d5eb7711c8418e235bf8
许霖 <l-xu22@mails.tsinghua.edu.cn>
2024-02-21 11:23:36 +0800

    搞定写好了创建活动的入口的前端，准备与后端对接

--- client/src/components/modules/ActivityCreateButton.js
+++ client/src/components/modules/ActivityCreateButton.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Papa from 'papaparse';
 
+import { post } from '../../utilities.js';
+import "./ActivityButton.css";
+
 const ActivityCreateButton = () => {
   const fileInputRef = React.useRef();
 
@@ -13,7 +16,22 @@ const ActivityCreateButton = () => {
     if (file) {
       Papa.parse(file, {
         complete: (results) => {
-          console.log(results.data);
+          for (const activity of results.data) {
+            const requestToPost = {
+              name: activity.名称,
+              description: activity.描述,
+              startTime: activity.开始时间,
+              endTime: activity.结束时间,
+              location: activity.地点,
+              deadline: activity.报名截止日期,
+              maxPeople: activity.人数上限,
+            };
+            /*post("/api/activity/create", {})
+            .catch((err) => {
+              alert(err);
+              return;
+            });*/
+          }
         },
         header: true,
       });
@@ -29,7 +47,9 @@ const ActivityCreateButton = () => {
         accept=".csv"
         onChange={handleFileChange}
       />
-      <button onClick={handleButtonClick}>打开文件</button>
+      <button onClick={handleButtonClick} className='ActivityCreateButton'>打开.csv文件以创建活动</button>
+      <h6>时间请使用国际标准时间格式,例如：2021-01-01T00:00:00.000Z</h6>
+      <h6>表头中应当按顺序排列这些信息：名称、描述、开始时间、结束时间、地点、报名截止日期、人数上限</h6>
     </div>
   );
 }
